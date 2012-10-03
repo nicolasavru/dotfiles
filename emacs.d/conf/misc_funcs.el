5692d8bab14510c00763d627452705390131dec3
Nicolas 'Rasputin' Avrutin <nicolasavru@gmail.com>
2012-10-03 13:21:11 -0400

    eval-and-replace

--- emacs.d/conf/misc_funcs.el
+++ emacs.d/conf/misc_funcs.el
@@ -15,3 +15,13 @@
                         (setq i (+ 32 i)) i (single-key-description i)
                         (setq i (+ 32 i)) i (single-key-description i)))
         (setq i (- i 96))))))
+
+(defun eval-and-replace ()
+  "Replace the preceding sexp with its value."
+  (interactive)
+  (backward-kill-sexp)
+  (condition-case nil
+      (prin1 (eval (read (current-kill 0)))
+             (current-buffer))
+    (error (message "Invalid expression")
+           (insert (current-kill 0)))))
