8924022df595c8025180b68cbf2ecfb68b088ab5
Nicolas Avrutin <nicolasavru@gmail.com>
2020-09-26 16:54:27 -0400

    Add create_symlinks script.

---
+++ create_symlinks.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ln -sf ~/dotfiles/emacs.d ~/.emacs.d
+ln -sf ~/dotfiles/emacs ~/.emacs
+
+ln -sf ~/dotfiles/xmonad ~/.xmonad
+ln -sf ~/dotfiles/xmobarrc ~/.xmobarrc
+
+ln -sf ~/dotfiles/zsh.d ~/.zsh.d
+ln -sf ~/dotfiles/zshenv ~/.zshenv
+ln -sf ~/dotfiles/zshrc  ~/.zshrc
+
+ln -s ~/dotfiles/Xdefaults ~/.Xdefaults
