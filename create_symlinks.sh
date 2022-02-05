63aa2cf77b123b333e6024ea1ecbae5b496fd8d9
Nicolas Avrutin <nicolas@avrutin.net>
2022-02-05 15:53:13 -0500

    treat existing directory symlniks corectly

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-ln -sf ~/dotfiles/emacs.d ~/.emacs.d
+ln -sfn ~/dotfiles/emacs.d ~/.emacs.d
 ln -sf ~/dotfiles/emacs ~/.emacs
 
 ln -sf ~/dotfiles/xmonad ~/.xmonad
 ln -sf ~/dotfiles/xmobarrc ~/.xmobarrc
 ln -sf ~/dotfiles/Xdefaults ~/.Xdefaults
 
-ln -sf ~/dotfiles/zsh.d ~/.zsh.d
+ln -sfn ~/dotfiles/zsh.d ~/.zsh.d
 ln -sf ~/dotfiles/zshenv ~/.zshenv
 ln -sf ~/dotfiles/zshrc  ~/.zshrc
 
-ln -sf ~/dotfiles/sway ~/.config/sway
-ln -sf ~/dotfiles/foot ~/.config/foot
+ln -sfn ~/dotfiles/sway ~/.config/sway
+ln -sfn ~/dotfiles/foot ~/.config/foot
 
-ln -sf ~/dotfiles/fontconfig ~/.config/fontconfig
+ln -sfn ~/dotfiles/fontconfig ~/.config/fontconfig
 
 ln -sf ~/dotfiles/bin ~/bin
