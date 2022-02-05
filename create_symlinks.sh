af7620ec1aa0709eefcf679b8b524f5b74a0fffb
Nicolas Avrutin <nicolas@avrutin.net>
2022-02-05 15:44:56 -0500

    add foot config and fontconfig

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -4,12 +4,15 @@ ln -sf ~/dotfiles/emacs ~/.emacs
 
 ln -sf ~/dotfiles/xmonad ~/.xmonad
 ln -sf ~/dotfiles/xmobarrc ~/.xmobarrc
-ln -s ~/dotfiles/Xdefaults ~/.Xdefaults
+ln -sf ~/dotfiles/Xdefaults ~/.Xdefaults
 
 ln -sf ~/dotfiles/zsh.d ~/.zsh.d
 ln -sf ~/dotfiles/zshenv ~/.zshenv
 ln -sf ~/dotfiles/zshrc  ~/.zshrc
 
 ln -sf ~/dotfiles/sway ~/.config/sway
+ln -sf ~/dotfiles/foot ~/.config/foot
+
+ln -sf ~/dotfiles/fontconfig ~/.config/fontconfig
 
 ln -sf ~/dotfiles/bin ~/bin
